818d6d241e7a7fb6da214ef3bb702f29ee481123
Yogesh Madhukar Dahake <M1020297@mindtree.com>
2019-05-19 18:40:05 +0530

    merging app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddGiftComponent } from './admin/add-gift/add-gift.component';
+import { HeaderComponent } from './common/header/header.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { environment } from 'src/environments/environment';
 import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './common/login/login.component';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddGiftComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule ,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    RouterModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
