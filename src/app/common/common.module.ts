89d38ffe28711899bf62b1ef3a35aa0ec1ecbc03
Payal Shah <M1043131@mindtree.com>
2019-05-19 12:08:40 +0530

    Added login component

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -3,8 +3,9 @@ import { HeaderComponent } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SearchBarComponent, CategoriesComponent, LayoutComponent],
+  declarations: [HeaderComponent, SearchBarComponent, CategoriesComponent, LayoutComponent, LoginComponent],
 })
 export class CommonModule { }
