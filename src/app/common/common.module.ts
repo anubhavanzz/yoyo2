79c1e83f86f56548f2dc2914c6186642fde379f5
Yogesh Madhukar Dahake <M1020297@mindtree.com>
2019-05-20 10:23:34 +0530

    merging code

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SearchBarComponent, CategoriesComponent, LayoutComponent, LoginComponent],
+  declarations: [SearchBarComponent, CategoriesComponent, LayoutComponent, LoginComponent, HeaderComponent],
+  exports: [SearchBarComponent, CategoriesComponent, LayoutComponent, LoginComponent, HeaderComponent]
 })
+
 export class CommonModule { }
