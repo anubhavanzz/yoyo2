34e9a5300ecf5fb6fda998c0fb6b0d0475222947
M1020297 <M1020297@A1ML18600>
2019-05-20 22:03:22 +0530

    merging confilict

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditGiftComponent } from './edit-gift/edit-gift.component';
 import { AddGiftComponent } from './add-gift/add-gift.component';
 import { MaterialModule } from 'src/app/common/material.module';
 import { FormsModule } from '@angular/forms';
+import { AllGiftsComponent } from './all-gifts/all-gifts.component';
+// import { CommonModule } from '../common/common.module';
+
 
 @NgModule({
-  declarations: [EditGiftComponent, AddGiftComponent],
+  declarations: [EditGiftComponent, AddGiftComponent, AllGiftsComponent ],
   imports: [
     CommonModule, MaterialModule, FormsModule
   ],
-  exports: [EditGiftComponent, AddGiftComponent]
+  exports: [EditGiftComponent, AddGiftComponent, AllGiftsComponent],
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AdminModule { }
