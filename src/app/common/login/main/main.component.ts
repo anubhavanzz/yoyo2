98878525f010ed4829ac55bf580f1169296d5ce1
anubhavanzz <anubhav.anzz@gmail.com>
2019-05-21 17:09:29 +0530

    Added Gift List common component for Home Page

--- src/app/common/login/main/main.component.ts
+++ src/app/common/login/main/main.component.ts
@@ -1,3 +1,4 @@
+import { GiftCard } from './../../../models/gift-card.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
+  public giftCard: GiftCard;
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.giftCard = {
+      $key: '',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Ye9g_Iw1YhVuXIniKGjdm6Yd8bAuUu46cEhPDpo1fJ2OgUId5Q',
+      points: 0,
+      description: '',
+      price: 100,
+      createdDate: '',
+      categoryName: '',
+      numberOfTimesBought: 7,
+      brand: '',
+      name: 'flipkart gift card'
+    };
   }
 
 }
