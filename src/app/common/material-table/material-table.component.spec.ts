27ed41609e4ab25948129fad9610aeb6b10ad062
Payal Shah  <payalShah93@gmail.com>
2019-05-24 23:55:02 +0530

    home page styilng

--- src/app/common/material-table/material-table.component.spec.ts
+++ src/app/common/material-table/material-table.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MaterialTableComponent } from './material-table.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { MatTableModule, MatPaginatorModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MaterialTableComponent', () => {
   let component: MaterialTableComponent;
@@ -8,7 +11,15 @@ describe('MaterialTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MaterialTableComponent ]
+      declarations: [
+        MaterialTableComponent,
+      ],
+      imports: [
+        MatTableModule,
+        MatPaginatorModule,
+        BrowserAnimationsModule
+      ]
+      // schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
--- src/app/common/nav/nav.component.spec.ts
+++ src/app/common/nav/nav.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatMenuModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 describe('NavComponent', () => {
   let component: NavComponent;
@@ -8,7 +13,22 @@ describe('NavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ NavComponent ],
+      imports: [
+        MatMenuModule,
+        AngularFireModule.initializeApp({
+          apiKey: 'AIzaSyC4O9LJigZttUlVbxpKJwrta3UmcRZp1Zg',
+          authDomain: 'yoyogift-e2fb7.firebaseapp.com',
+          databaseURL: 'https://yoyogift-e2fb7.firebaseio.com',
+          projectId: 'yoyogift-e2fb7',
+          storageBucket: 'yoyogift-e2fb7.appspot.com',
+          messagingSenderId: '293963158537',
+          appId: '1:293963158537:web:adff0773339da453'
+        }),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
   }));
