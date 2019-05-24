0f6abd89b6c23409123b0d9044af7ce5a977e3f7
Payal Shah  <payalShah93@gmail.com>
2019-05-24 18:11:45 +0530

    Test bed errors fixed

--- src/app/common/category-list/category-list.component.spec.ts
+++ src/app/common/category-list/category-list.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoryListComponent } from './category-list.component';
+import { MatListModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 describe('CategoryListComponent', () => {
   let component: CategoryListComponent;
@@ -8,7 +12,21 @@ describe('CategoryListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoryListComponent ]
+      declarations: [ CategoryListComponent ],
+      imports: [
+        MatListModule,
+        AngularFireModule.initializeApp({
+          apiKey: 'AIzaSyC4O9LJigZttUlVbxpKJwrta3UmcRZp1Zg',
+          authDomain: 'yoyogift-e2fb7.firebaseapp.com',
+          databaseURL: 'https://yoyogift-e2fb7.firebaseio.com',
+          projectId: 'yoyogift-e2fb7',
+          storageBucket: 'yoyogift-e2fb7.appspot.com',
+          messagingSenderId: '293963158537',
+          appId: '1:293963158537:web:adff0773339da453'
+        }),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+      ]
     })
     .compileComponents();
   }));
--- src/app/common/gift-list/gift-list.component.spec.ts
+++ src/app/common/gift-list/gift-list.component.spec.ts
@@ -1,14 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GiftListComponent } from './gift-list.component';
+import { GiftCardComponent } from '../gift-card/gift-card.component';
+import { MatCardModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { ToastrService } from 'ngx-toastr';
+import { GiftDetailDispatcher } from '../store/gift-details-store/gift-details.dispatcher';
+import { Store } from '@ngrx/store';
+import { Observable, from as observableFrom } from 'rxjs';
+// import { from as observableFrom } from 'rxjs';
 
+class MockService {
+  public select(mapFn: any, ...paths: string[]): Observable<any> {
+    const val = {
+      globalState: {
+        transactionInitState: '',
+        accountAmountState: ''
+      }
+    };
+    return observableFrom([val]);
+  }
+  public dispatch(): any {}
+}
 describe('GiftListComponent', () => {
   let component: GiftListComponent;
   let fixture: ComponentFixture<GiftListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GiftListComponent ]
+      declarations: [ GiftListComponent, GiftCardComponent ],
+      imports: [
+        MatCardModule,
+        AngularFireModule.initializeApp({
+          apiKey: 'AIzaSyC4O9LJigZttUlVbxpKJwrta3UmcRZp1Zg',
+          authDomain: 'yoyogift-e2fb7.firebaseapp.com',
+          databaseURL: 'https://yoyogift-e2fb7.firebaseio.com',
+          projectId: 'yoyogift-e2fb7',
+          storageBucket: 'yoyogift-e2fb7.appspot.com',
+          messagingSenderId: '293963158537',
+          appId: '1:293963158537:web:adff0773339da453'
+        }),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
+      ],
+      providers: [ToastrService, GiftDetailDispatcher,
+        { provide: Store, useClass: MockService },
+      ]
     })
     .compileComponents();
   }));
