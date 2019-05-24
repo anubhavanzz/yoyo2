954c76cc1c07f84a5e21b9ac1ceb3176d8ceac7e
Payal Shah  <payalShah93@gmail.com>
2019-05-24 12:45:57 +0530

    test bed set up add-category

--- src/app/admin/add-category/add-category.component.spec.ts
+++ src/app/admin/add-category/add-category.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddCategoryComponent } from './add-category.component';
+import { FormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatTableModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
 
 describe('AddCategoryComponent', () => {
   let component: AddCategoryComponent;
@@ -8,7 +15,23 @@ describe('AddCategoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddCategoryComponent ]
+      declarations: [ AddCategoryComponent ],
+      imports: [FormsModule,
+        ToastrModule.forRoot(),
+         MatTableModule,
+         AngularFireModule.initializeApp({
+          apiKey: 'AIzaSyC4O9LJigZttUlVbxpKJwrta3UmcRZp1Zg',
+          authDomain: 'yoyogift-e2fb7.firebaseapp.com',
+          databaseURL: 'https://yoyogift-e2fb7.firebaseio.com',
+          projectId: 'yoyogift-e2fb7',
+          storageBucket: 'yoyogift-e2fb7.appspot.com',
+          messagingSenderId: '293963158537',
+          appId: '1:293963158537:web:adff0773339da453'
+        }),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule                     ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [ToastrService]
     })
     .compileComponents();
   }));
