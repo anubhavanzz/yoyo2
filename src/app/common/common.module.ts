5412ee07c435b7e369d0029a61acff3fac6cdfc1
Payal Shah  <payalShah93@gmail.com>
2019-05-24 12:03:17 +0530

    Merging changes

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,7 +2,6 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { LayoutComponent } from './layout/layout.component';
-import { LoginComponent } from './login/login.component';
 import { IntroductionContentComponent } from './introductionContent/introductionContent.component';
 import { MainComponent } from './login/main/main.component';
 import { NavComponent } from './nav/nav.component';
@@ -15,17 +14,18 @@ import { CategoryListComponent } from './category-list/category-list.component';
 import { MaterialTableComponent } from './material-table/material-table.component';
 import { GiftDetailDispatcher } from './store/gift-details-store/gift-details.dispatcher';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './login/home.component';
 
 
 @NgModule({
   declarations: [SearchBarComponent, CategoriesComponent,
-    LayoutComponent, LoginComponent, IntroductionContentComponent, MainComponent, NavComponent, FooterComponent,
+    LayoutComponent, HomeComponent, IntroductionContentComponent, MainComponent, NavComponent, FooterComponent,
     GiftCardComponent,
     GiftListComponent,
     CategoryListComponent,
     GiftListComponent, MaterialTableComponent
   ],
-  exports: [SearchBarComponent, CategoriesComponent, LayoutComponent, LoginComponent, IntroductionContentComponent,
+  exports: [SearchBarComponent, CategoriesComponent, LayoutComponent, HomeComponent, IntroductionContentComponent,
     GiftCardComponent, NavComponent, FooterComponent, MaterialTableComponent
 
   ],
