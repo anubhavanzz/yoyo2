7e5d77d13df1ecd51a1e44244d73f94a98ad8f4d
M1020297 <M1020297@A1ML18600>
2019-05-25 21:52:29 +0530

    Added toaster notifciation on can activate guard

--- src/app/common/services/auth-guard.service.ts
+++ src/app/common/services/auth-guard.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router,
+    private tostr: ToastrService) { }
 
   canActivate() {
 
-    console.log('From canActivate guard :User getting validated here is :' , this.authService.user);
+    console.log('From canActivate guard :User getting validated here is :', this.authService.user);
     if (this.authService.user.email) {
       return true;
     }
+
+    this.tostr.warning('Please Log in');
     this.router.navigate(['/']);
     return false;
 
