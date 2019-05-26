a5706874d09f64204f19b9a4c6c12faaeaedff2a
M1020297 <M1020297@A1ML18600>
2019-05-26 16:17:23 +0530

    Added ngonDestroy for Admin module

--- src/app/admin/manage-users/manage-users.component.ts
+++ src/app/admin/manage-users/manage-users.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FirebaseService } from 'src/app/common/services/firebase.service';
 import { User } from 'src/app/models/user.model';
 import { MatTableColumns } from 'src/app/common/MatTableColumns';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
   styleUrls: ['./manage-users.component.css']
 })
-export class ManageUsersComponent implements OnInit {
+export class ManageUsersComponent implements OnInit ,OnDestroy {
 
   usersArray: User[];
+  subscriptions: Subscription[] = [];
   // ColumnDef, HeaderCellDef
   matTableColumns: MatTableColumns[] = [
     new MatTableColumns('name', 'User Name'),
@@ -27,15 +29,21 @@ export class ManageUsersComponent implements OnInit {
 
   ngOnInit() {
 
-    this.fbService.getAllUsersFromFirebase().subscribe(list => {
-      this.usersArray = list.map(item => {
-        return {
-          $key: item.key,
-          ...item.payload.val()
-        };
-      });
-    });
+    this.subscriptions.push(
+      this.fbService.getAllUsersFromFirebase().subscribe(list => {
+        this.usersArray = list.map(item => {
+          return {
+            $key: item.key,
+            ...item.payload.val()
+          };
+        });
+      })
+    );
   }
 
-
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
+  }
 }
