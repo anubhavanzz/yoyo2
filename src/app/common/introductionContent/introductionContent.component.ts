b15bbfd43e90ac6f4d172be37638c6ee16413aef
Payal Shah  <payalShah93@gmail.com>
2019-05-26 20:46:13 +0530

    I18N

--- src/app/common/introductionContent/introductionContent.component.ts
+++ src/app/common/introductionContent/introductionContent.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-introduction-content',
@@ -9,8 +10,9 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class IntroductionContentComponent implements OnInit, AfterViewInit {
   user: firebase.User;
-  constructor(private afAuth: AngularFireAuth, private spinner: NgxSpinnerService) {
+  constructor(private afAuth: AngularFireAuth, private spinner: NgxSpinnerService, private translate: TranslateService) {
     this.afAuth.authState.subscribe(user => this.user = user);
+    translate.getDefaultLang();
   }
 
   ngOnInit() {
